577668a62228a013c5507b70a95da5a458ac8e39
Christopher Le <christopherle@ChristophersAir.home>
2017-07-15 08:55:24 -0700

    third push

--- liri.js
+++ liri.js
@@ -6,62 +6,64 @@ var fs = require("fs");
 
 //-Twitter----------------------------------------------------------------------
 
-//
-// function myTweets() {
-//
-// var client = new Twitter({
-// 	  consumer_key: keys.twitterKeys.consumer_key,
-// 	  consumer_secret: keys.twitterKeys.consumer_secret,
-// 	  access_token_key: keys.twitterKeys.access_token_key,
-// 	  access_token_secret: keys.twitterKeys.access_token_secret
-// 	});
-//
-// var params = {screen_name: 'grizz_le', count: 20};
-// client.get('statuses/user_timeline', params, function(error, tweets, response) {
-//   if (!error) {
-//     console.log("\n---------------------\n");
-//     for (var i = 0; i < tweets.length; i++) {
-//     console.log(tweets);
-//       }
-//     }
-//   });
-// }
-// if(process.argv[2] === "my-tweets"){
-//   myTweets();
-// }
+
+function myTweets() {
+
+var client = new Twitter({
+	  consumer_key: keys.twitterKeys.consumer_key,
+	  consumer_secret: keys.twitterKeys.consumer_secret,
+	  access_token_key: keys.twitterKeys.access_token_key,
+	  access_token_secret: keys.twitterKeys.access_token_secret
+	});
+
+var params = {screen_name: 'grizz_le', count: 20};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log("\n---------------------\n");
+    for (var i = 0; i < tweets.length; i++) {
+    console.log(tweets[i].user.screen_name)
+    console.log(tweets[i].text);
+    console.log("\n---------------------\n");
+      }
+    }
+  });
+}
+if(process.argv[2] === "my-tweets"){
+  myTweets();
+}
 
 //Spotify-----------------------------------------------------------------------
 
-function mySpotify(song) {
-
-  if (!song) {
-    song = "The Sign";
-  }
-
-  var spotify = new Spotify({
-    id: keys.spotifyKeys.id,
-    secret: keys.spotifyKeys.secret
-  });
-
-  spotify.search({ type: 'track', query: song}, function(err, data) {
-    if (err) {
-      return console.log('Error occurred: ' + err);
-    }
-  for (var i = 0; i < 10; i++) {
-  console.log("\n---------------------\n");
-  console.log(data.tracks.items[0].artists[0].name);
-  console.log(data.tracks.items[i].name);
-  console.log(data.tracks.items[i].preview_url);
-  console.log(data.tracks.items[i].album.name);
-  console.log("\n---------------------\n");
-  }
-    });
-
-}
-
-if(process.argv[2] === "spotify-this-song"){
-  mySpotify();
-}
+// function mySpotify(song) {
+//
+//   if (!song) {
+//     song = "The Sign";
+//   }
+//
+//   var spotify = new Spotify({
+//     id: keys.spotifyKeys.id,
+//     secret: keys.spotifyKeys.secret
+//   });
+//
+//   spotify.search({ type: 'track', query: song}, function(err, data) {
+//     if (err) {
+//       return console.log('Error occurred: ' + err);
+//     }
+//   for (var i = 0; i < 10; i++) {
+//   console.log("\n---------------------\n");
+//   console.log(data.tracks.items[0].artists[0].name);
+//   console.log(data.tracks.items[i].name);
+//   console.log(data.tracks.items[i].preview_url);
+//   console.log(data.tracks.items[i].album.name);
+//   console.log("\n---------------------\n");
+//   }
+//     });
+//
+// }
+//
+// if(process.argv[2] === "spotify-this-song"){
+//   mySpotify();
+// }
 
 
 //-Movie------------------------------------------------------------------------
